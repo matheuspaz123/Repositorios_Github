92afe274fb612495d496c0e950bdd0aa2e6eb082
matheuspaz123 <matheuspaz30071998@gmail.com>
2021-03-25 20:09:09 -0300

    Classe para receber o json e para reservar endereçoes feitas

--- app/src/main/java/com/example/repositoriosgithub/GitModel.kt
+++ app/src/main/java/com/example/repositoriosgithub/GitModel.kt
@@ -3,4 +3,4 @@ package com.example.repositoriosgithub
 //estrutura dos atributos da mesma hierarquia do json do github
 data class GitModel (val item: Items)
 data class Items(val name: String, val owner: Owner, val description: String)
-data class Owner(val login: String,val avatar_ur: String)
+data class Owner(val login: String,val avatar_ur: String )
