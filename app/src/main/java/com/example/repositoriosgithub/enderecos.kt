62434a7b38511d12a38c5035ea4104c66086065f
matheuspaz123 <matheuspaz30071998@gmail.com>
2021-03-25 20:08:45 -0300

    Classe para receber o json e para reservar endereçoes feitas

---
+++ app/src/main/java/com/example/repositoriosgithub/enderecos.kt
@@ -0,0 +1,13 @@
+package com.example.repositoriosgithub
+
+
+//classe para as url's da api do github
+
+
+class enderecos {
+
+    val urlBase: String = "https://api.github.com/"
+    val repositoriosPublicos: String = "repositories"
+
+
+}
