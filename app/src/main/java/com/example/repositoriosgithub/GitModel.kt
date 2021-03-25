1bafe8b0065d6ca0b5d1f847bfacc0bfc605e72e
matheuspaz123 <matheuspaz30071998@gmail.com>
2021-03-25 20:24:03 -0300

    Classe para receber o json e para reservar endereçoes feitas

--- app/src/main/java/com/example/repositoriosgithub/GitModel.kt
+++ app/src/main/java/com/example/repositoriosgithub/GitModel.kt
@@ -1,6 +1,7 @@
 package com.example.repositoriosgithub
 
 //estrutura dos atributos da mesma hierarquia do json do github
+
 data class GitModel (val item: Items)
 data class Items(val name: String, val owner: Owner, val description: String)
 data class Owner(val login: String,val avatar_ur: String )
--- app/src/main/java/com/example/repositoriosgithub/enderecos.kt
+++ app/src/main/java/com/example/repositoriosgithub/enderecos.kt
@@ -3,7 +3,6 @@ package com.example.repositoriosgithub
 
 //classe para as url's da api do github
 
-
 class enderecos {
 
     val urlBase: String = "https://api.github.com/"
